f1e35e2d46a6e2fdbfeddab104a104a715c99acc
LimSumi <thddkwl5330@naver.com>
2023-04-24 21:38:33 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,10 +9,13 @@ def do_fizzbuzz():
     for i in range(1.num+1):
         if i%3==0:
             print('fizz')
+        elif:
+            i%5==0:
+                print('buzz')
         else:
             print(f'{i}')
 
 
 
 if __name__=='__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
